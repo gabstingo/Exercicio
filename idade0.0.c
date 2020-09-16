4176734cb48f26a3cc45b8d31f456abc3cf1bcf4
gabstingo <71369165+gabstingo@users.noreply.github.com>
2020-09-16 04:10:57 -0300

    Create idade0.0.c

---
+++ idade0.0.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	int ano, idade;
+	char nome[15];
+	
+	printf("Dgite seu nome:");
+	scanf("%s",&nome);
+	
+	printf("agora digite o ano do seu nascimento:");
+	scanf("%i", &ano);
+	
+	return 0;
+}
